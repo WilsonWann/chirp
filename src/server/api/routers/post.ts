53abab325648f412b418662da24a139baf0667b1
WilsonWann <genius19880825@hotmail.com>
2024-02-15 16:23:34 +0800

    working posts

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -4,7 +4,7 @@ import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
 
@@ -16,36 +16,58 @@ const filterUserForClient = (user: User) => {
 }
 
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(async ({ ctx }) => {
-    const posts = await ctx.db.post.findMany({
-      take: 100,
-    });
+  getAll: publicProcedure
+    .query(async ({ ctx }) => {
+      const posts = await ctx.db.post.findMany({
+        take: 100,
+        orderBy: [
+          { createdAt: "desc" },
+        ]
+      });
 
-    const users = (
-      await clerkClient.users.getUserList({
-        userId: posts.map(post => post.authorId),
-        limit: 100,
-      })
-    ).map(filterUserForClient);
-
-    console.log("🚀 ~ getAll:publicProcedure.query ~ users:", users)
-
-    return posts.map(post => {
-      const author = users.find(user => user.id === post.authorId)
-
-      if (!author || !author.username)
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Author for post not found"
+      const users = (
+        await clerkClient.users.getUserList({
+          userId: posts.map(post => post.authorId),
+          limit: 100,
         })
+      ).map(filterUserForClient);
 
-      return {
-        post,
-        author: {
-          ...author,
-          username: author.username
+      console.log("🚀 ~ getAll:publicProcedure.query ~ users:", users)
+
+      return posts.map(post => {
+        const author = users.find(user => user.id === post.authorId)
+
+        if (!author || !author.username)
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Author for post not found"
+          })
+
+        return {
+          post,
+          author: {
+            ...author,
+            username: author.username
+          },
+        }
+      })
+    }),
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().emoji().min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+
+      const post = await ctx.db.post.create({
+        data: {
+          authorId,
+          content: input.content,
         },
-      }
+      });
+
+      return post;
     })
-  })
 });
